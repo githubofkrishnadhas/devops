7693e33913d9b63141077c52e9ffc42f39262ca4
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2022-07-05 01:49:14 +0530

    terraform_rg removed

--- terraform_rg/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_name" {
-  default       = "ARCHITECTS-DEV-RG"
-  description   = "RG Name"
-}
-
-variable "resource_group_location" {
-  default = "South India"
-  description   = "Location of the resource group."
-}
